1f2560e792f026499e8378f05f036f10d938033c
Eléonore Breton <eleonore@Air-de-Eleonore.home>
2021-06-14 08:55:24 +0200

    config recommended here: https://medium.com/@eth3rnit3/create-react-app-rails-6-0-custom-server-deployment-part-1-development-configuration-92ce11ea9c56

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,8 @@ Rails.application.routes.draw do
     end
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  get '*path', to: 'application#frontend_index_html', constraints: lambda { |request|
+      !request.xhr? && request.format.html?
+    }
 end
