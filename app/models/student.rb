ee4f641b6d38c6c15b89b12b6da1f2b2bc9f5da3
Eléonore Breton <eleonore.breton@hec.edu>
2021-06-21 20:40:06 +0200

    add comment for reminder

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,5 +1,5 @@
 class Student < ApplicationRecord
   belongs_to :city, optional: true
-  has_many :choices
+  has_many :choices # max 6, how to put a validation for this condition?
   # role will be set with cancancan
 end
