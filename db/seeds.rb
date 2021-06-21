bcc53cd7efbeec292f87ec605ab21b982d58c660
Eléonore Breton <eleonore.breton@hec.edu>
2021-06-21 13:11:25 +0200

    add real data to db

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,23 +1,34 @@
-#### TO DO ###
-# Parse Anna'a excel file to get all cities & seats
-##############
+require 'csv'
 
-City.create(
-	name: 'Aix-en-Provence',
-	nb_seats: 4
-	)
+City.all.destroy_all
 
-City.create(
-	name: 'Marseille',
-	nb_seats: 6
-	)
+source_file = "/Users/eleonore/Documents/Code/enm-city-allocator/db/enm_cities_seats.csv"
 
-City.create(
-	name: 'Paris',
-	nb_seats: 6
-	)
+CSV.foreach(source_file) do |row|
+	City.create(
+		name: row[1],
+		nb_seats: row[2]
+		)
+end
 
-City.create(
-	name: 'Angers',
-	nb_seats: 3
-	)
+#p City.all
+
+# City.create(
+# 	name: 'Aix-en-Provence',
+# 	nb_seats: 4
+# 	)
+
+# City.create(
+# 	name: 'Marseille',
+# 	nb_seats: 6
+# 	)
+
+# City.create(
+# 	name: 'Paris',
+# 	nb_seats: 6
+# 	)
+
+# City.create(
+# 	name: 'Angers',
+# 	nb_seats: 3
+# 	)
