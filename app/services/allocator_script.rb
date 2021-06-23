a0b6b9f2d4284cfd6e16812557a6575bf96f4a9f
Eléonore Breton <eleonore.breton@hec.edu>
2021-06-23 14:26:04 +0200

    working on the script

--- app/services/allocator_script.rb
+++ app/services/allocator_script.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # this has to be launched in the console with load 'app/services/allocator_script.rb'
 
 # best practice Pierre: declare a class and then launch the method on the class
@@ -9,20 +11,18 @@
 
 # STEP #1 - grant all first choices that are not disputed
 
-# SELECT cities.name, cities.nb_seats, count(choices.id) 
-# FROM cities INNER JOIN choices ON cities.id = choices.city_id 
-# WHERE choices.rank = 1 
+# SELECT cities.name, cities.nb_seats, count(choices.id)
+# FROM cities INNER JOIN choices ON cities.id = choices.city_id
+# WHERE choices.rank = 1
 # GROUP BY cities.name, cities.nb_seats;
 
 p Choice.all.count
-grantable_first_choices = Choice.all.select { |choice| choice.rank == 1}
+grantable_first_choices = Choice.all.select { |choice| choice.rank == 1 }
 p grantable_first_choices.size
-
 
 # STEP #2 - do the same for second to last choices
 
 # STEP #3 - Use brute force for remanining possibilites
-
 
 #############################
 ###### SECOND METHOD ########
@@ -39,7 +39,7 @@ p grantable_first_choices.size
 # STEP #2 - do the same for second to last choices
 
 # STEP #3 - rank remaining students
-# a student has a high rank if he/she asked for cities that many people asked for. 
+# a student has a high rank if he/she asked for cities that many people asked for.
 # As these students run the risk of having a consequent impact on the sum of distances, we give them in priority one of their first choices
 
 #############################
