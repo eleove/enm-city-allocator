2727faeeb94841d7b165ebf5012526b658d0bda8
Eléonore Breton <eleonore.breton@hec.edu>
2021-06-23 14:50:14 +0200

    add dependency and validation (Pierre's review)

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,5 +1,9 @@
+# frozen_string_literal: true
+
 class Student < ApplicationRecord
   belongs_to :city, optional: true
-  has_many :choices # max 6, how to put a validation for this condition?
+  has_many :choices, dependent: :destroy
+
+  validates :choices, length: {  maximum: 6	}
   # role will be set with cancancan
 end
