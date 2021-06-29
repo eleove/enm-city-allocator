ef08b94dded61fbfe39faf8f9a4eb391f00cae03
Eléonore Breton <eleonore.breton@hec.edu>
2021-06-29 20:28:56 +0200

    finish seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,7 +17,7 @@ CSV.foreach(source_file, headers: true) do |row|
   )
 end
 
-# # add students in DB
+# add students in DB
 
 Student.find_each(&:destroy!)
 
@@ -33,24 +33,25 @@ end
 
 Choice.find_each(&:destroy!)
 
-# itérer sur les voeux, trouver 1 student qui n'a pas ce voeu là (à ce rank) et passer au voeu suivant
-# use find_by! instead of find_by
+City.order("nb_seats DESC").all.each do |city|
+  city.wishes.each_with_index do |wish, ind|
+    # next if wish.zero?
 
-# Student.all.each do |student|
-# 	rank = 1
-# 	6.times {
-# 		n = rand(100)
-# 		if n < 20
-# 			random_city = City.find_by name: 'PARIS'
-# 		elsif n < 40
-# 			random_city = City.find_by name: 'BORDEAUX'
-# 		else
-# 			random_city = City.all.sample
-# 		end
-# 		Choice.create!(
-# 			city: random_city,
-# 			student: student,
-# 			rank: rank)
-# 		rank += 1
-# 	}
-# end
+    until wish.zero?
+
+      students = Student.left_outer_joins(:choices)
+      selected_students = students.where(choices: { id: nil }).or(students.where.not(id: Choice.where(rank: ind).select(:student_id)).where.not(id: Choice.where(city: city).select(:student_id)))
+      # https://stackoverflow.com/questions/49347632/rails-query-join-with-where-not-doesnt-work
+
+      selected_student = selected_students.select { |student| student.choices.length < 6 }.sample
+      new_choice = Choice.create!(
+        city: city,
+        student: selected_student,
+        rank: ind
+      )
+      selected_student.save!
+      wish -= 1
+      city.save!
+    end
+  end
+end
