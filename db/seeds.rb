f11042195183b2d385446551de9e59a4dff768b4
Eléonore Breton <eleonore.breton@hec.edu>
2021-07-08 16:04:53 +0200

    improve seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -33,22 +33,22 @@ end
 
 Choice.find_each(&:destroy!)
 
-City.order("nb_seats DESC").all.each do |city|
-  city.wishes.each_with_index do |wish, ind|
-    # next if wish.zero?
-
+City.order('nb_seats DESC').all.each do |city|
+  city.wishes.each_with_index do |wish, index|
     until wish.zero?
 
       students = Student.left_outer_joins(:choices)
-      selected_students = students.where(choices: { id: nil }).or(students.where.not(id: Choice.where(rank: ind).select(:student_id)).where.not(id: Choice.where(city: city).select(:student_id)))
+      selected_students = students.where(choices: { id: nil }).or(students.where.not(id: Choice.where(rank: index).select(:student_id)).where.not(id: Choice.where(city: city).select(:student_id)))
       # https://stackoverflow.com/questions/49347632/rails-query-join-with-where-not-doesnt-work
 
       selected_student = selected_students.select { |student| student.choices.length < 6 }.sample
-      new_choice = Choice.create!(
+      Choice.create!(
         city: city,
         student: selected_student,
-        rank: ind
+        rank: index
       )
+      p "There are now #{Choice.count} choices in the db"
+      # above line can be removed but useful as this seeding part takes several minutes
       selected_student.save!
       wish -= 1
       city.save!
